4890518b3869be330ed10ace5ce71464e36ee497
Kurt Van Etten <kurt@vanetten.us>
2020-01-12 20:12:23 -0500

    Add table definitions for projects_resources and tasks tables

--- data/migrations/20200112194831_initial.js
+++ data/migrations/20200112194831_initial.js
@@ -34,10 +34,49 @@ exports.up = async function(knex) {
 
   // projects_resources table
   // links projects and resources tables in many-many relationship
+  await knex.schema.createTable('projects_resources', table => {
+    table
+      .integer('project_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('projects')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+    table
+      .integer('resource_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('resources')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+    // create primary key - force each project_id/resource_id combo to be unique
+    table.primary(['project_id', 'resource_id']);
+  });
 
   // tasks table
+  // each task is linked to exactly one project in the projects table
+  await knex.schema.createTable('tasks', table => {
+    table.increments('id');
+    table
+      .integer('project_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('projects')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+    table.string('description').notNullable();
+    table.string('notes');
+    table
+      .boolean('completed')
+      .notNullable()
+      .defaultTo(false);
+  });
 };
 
+// drop tables in the reverse order of how they were created
 exports.down = async function(knex) {
   await knex.schema.dropTableIfExists('tasks');
   await knex.schema.dropTableIfExists('projects_resources');
